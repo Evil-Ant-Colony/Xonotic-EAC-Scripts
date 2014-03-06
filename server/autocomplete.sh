eef10d02be7cf7fb39a4a9873126d8b055765b39
Mattia Basaglia <mattia.basaglia@gmail.com>
2014-03-06 19:02:10 +0100

    sv: tmux exec

--- server/autocomplete.sh
+++ server/autocomplete.sh
@@ -6,12 +6,12 @@ _eac_server_autocomplete()
 	_get_comp_words_by_ref cur
 
 	case ${COMP_WORDS[1]} in
-		ls|list|cmd|exec|irc|dump)
+		ls|list|cmd|exec|irc|dump|quit|start|restart)
 			COMPREPLY=( $( compgen -W "$(./server)" -- "$cur" ) )
 			return 0
 		;;
 	esac
 
-	COMPREPLY=( $( compgen -W 'inline ls list cmd exec irc view dump register' -- "$cur" ) )
+	COMPREPLY=( $( compgen -W 'inline ls list cmd exec irc view dump register quit start restart' -- "$cur" ) )
 }
 complete -F _eac_server_autocomplete server
