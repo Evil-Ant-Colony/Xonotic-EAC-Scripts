89aa8010d08f8623c16ea3762987166629a07317
Mattia Basaglia <mattia.basaglia@gmail.com>
2014-10-03 20:21:07 +0200

    Fix scripts

--- server/autocomplete.sh
+++ server/autocomplete.sh
@@ -4,14 +4,20 @@ _eac_server_autocomplete()
 
 	COMPREPLY=()
 	_get_comp_words_by_ref cur
-
-	case ${COMP_WORDS[1]} in
-		ls|list|cmd|exec|irc|dump|quit|start|restart)
-			COMPREPLY=( $( compgen -W "$(./server)" -- "$cur" ) )
-			return 0
-		;;
-	esac
-
-	COMPREPLY=( $( compgen -W 'inline ls list cmd exec irc view dump register quit start restart' -- "$cur" ) )
+	
+	
+	COMPREPLY=( $( compgen -W "$(server autocomplete ${COMP_WORDS[1]})" -- "$cur" ) )
+# 	eac_server_commands=$(server commands)
+# 	
+# 	for eac_server_cmd in $eac_server_commands
+# 	do
+# 		if [ "${COMP_WORDS[1]}" = "$eac_server_cmd" ]
+# 		then
+# 			COMPREPLY=( $( compgen -W "$(server inline)" -- "$cur" ) )
+# 			return 0
+# 		fi
+# 	done
+# 
+# 	COMPREPLY=( $( compgen -W "$eac_server_commands" -- "$cur" ) )
 }
 complete -F _eac_server_autocomplete server
